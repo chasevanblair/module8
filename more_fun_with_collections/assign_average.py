ab57f558d46a74e80ded2d22fa4b2c86f04bc698
chasevanblair <crvanblair@dmacc.edu>
2020-06-28 01:45:52 -0500

    dict switch final

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,4 +1,24 @@
+"""
+
+Program: assign_average.py
+
+Author: Chase Van Blair
+
+Last date modified: 6/28/20
+
+
+The purpose of this program is to create an artificial switch case in python
+
+"""
+
+
 def switch_average(user_dict, key):
+    """
+    this function emulates a switch function and either returns the item or none if not in dict
+    :param user_dict: dict to be used for case
+    :param key: key that determines how case runs
+    :return: either value of the key or none
+    """
     user_key = str(key).upper()
     if user_key in user_dict:
         if user_key == 'A':
--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -3,13 +3,13 @@ import unittest
 
 Program: test_assign_average.py
 
-Author: Your Name
+Author: Chase Van Blair
 
-Last date modified: MM/DD/YYYY
+Last date modified: 6/28/20
 
 
-The purpose of this program is to accept any input, 
-convert to a integer type and output the integer. 
+The purpose of this program is to test the artificial switch
+case made in assign_average.py
 
 """
 
@@ -19,31 +19,55 @@ from more_fun_with_collections.assign_average import switch_average
 
 class TestAverage(unittest.TestCase):
     def test_a(self):
+        """
+        test switch_average() when passed A
+        :return:
+        """
         user_dict = {'A': 3, 'B': 4, 'C': 5, 'D': 6, 'E': 7}
         user_key = 'a'
         self.assertEqual(3, switch_average(user_dict, user_key.upper()))
 
     def test_b(self):
+        """
+        test switch_average() when passed b
+        :return:
+        """
         user_dict = {'A': 3, 'B': 4, 'C': 5, 'D': 6, 'E': 7}
         user_key = 'B'
         self.assertEqual(4, switch_average(user_dict, user_key.upper()))
 
     def test_c(self):
+        """
+        test switch_average() when passed c
+        :return:
+        """
         user_dict = {'A': 3, 'B': 4, 'C': 5, 'D': 6, 'E': 7}
         user_key = 'C'
         self.assertEqual(5, switch_average(user_dict, user_key.upper()))
 
     def test_d(self):
+        """
+        test switch_average() when passed d
+        :return:
+        """
         user_dict = {'A': 3, 'B': 4, 'C': 5, 'D': 6, 'E': 7}
         user_key = 'd'
         self.assertEqual(6, switch_average(user_dict, user_key.upper()))
 
     def test_e(self):
+        """
+        test switch_average() when passed e
+        :return:
+        """
         user_dict = {'A': 3, 'B': 4, 'C': 5, 'D': 6, 'E': 7}
         user_key = 'e'
         self.assertEqual(7, switch_average(user_dict, user_key.upper()))
 
     def test_none(self):
+        """
+        test switch_average() when passed a key thats not in the dict
+        :return:
+        """
         user_dict = {'A': 3, 'B': 4, 'C': 5, 'D': 6, 'E': 7}
         user_key = ''
         self.assertEqual(None, switch_average(user_dict, user_key.upper()))
