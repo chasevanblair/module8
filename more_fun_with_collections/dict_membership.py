e7881c3da2656e20fe65dc832a014bef09a78d63
chasevanblair <crvanblair@dmacc.edu>
2020-06-27 23:45:02 -0500

    dict final

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -1,3 +1,23 @@
-def in_dict(d, key):
-    return key in d
+"""
 
+Program: dict_membership.py
+
+Author: Chase Van Blair
+
+Last date modified: 6/27/20
+
+
+The purpose of this program is to get familiar with dictionaries
+by checking if a key is in a dict
+
+"""
+
+
+def in_dict(d, key):
+    """
+    checks if key is a key in dictionary d
+    :param d: dictionary to be checked
+    :param key: key that func looks for
+    :return: true or false of key in d
+    """
+    return key in d
--- test/test_dict_membership.py
+++ test/test_dict_membership.py
@@ -1,3 +1,16 @@
+"""
+
+Program: test_dict_membership.py
+
+Author: Chase Van Blair
+
+Last date modified: 6/27/20
+
+
+The purpose of this program is to get familiar with dictionaries
+by testing the functions in dict_membership
+
+"""
 import unittest
 
 from more_fun_with_collections.dict_membership import in_dict
@@ -5,9 +18,17 @@ from more_fun_with_collections.dict_membership import in_dict
 
 class TestDict(unittest.TestCase):
     def test_in_dict_true(self):
+        """
+        test a true return from in_dict()
+        :return:
+        """
         my_dict = {'A': 3, 'B': 44, 'C': 8}
         self.assertTrue(in_dict(my_dict, 'A'))
 
     def test_in_dict_false(self):
+        """
+        test a false return from in_dict()
+        :return:
+        """
         my_dict = {'A': 3, 'B': 44, 'C': 8}
         self.assertFalse(in_dict(my_dict, 'D'))
